c0861d0341b483de00149dccc3bdc7669edba550
chadbanghart <cbanghar@gmail.com>
2024-06-04 15:25:18 -0700

    finish auth signup, add journal model and journal index page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('accounts/signup', views.signup, name='signup'),
   path('', views.home, name='home'),
+  path('journals/', views.JournalList.as_view(), name='journals_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views.generic import ListView
+from .models import Journal
 
 # Create your views here.
+# @login_required -> auth decorator for view functions
+# LoginRequiredMixin -> auth mixin for CBV
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
 def home(request):
   return render(request, 'home.html')
+
+class JournalList(LoginRequiredMixin, ListView):
+  model = Journal
