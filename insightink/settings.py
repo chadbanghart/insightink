d1422b6d1d40ddb4ba10dc902f41ff06b8216252
Taylor Stallcup <tstallcup321@gmail.com>
2024-06-04 16:31:39 -0500

    created super user, login, and logout

--- insightink/settings.py
+++ insightink/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# successful login redirect
+LOGIN_REDIRECT_URL = '/'
+# logout
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
